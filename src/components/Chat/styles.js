74f1134192f2141df49d792a07d4e782f1947f12
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2024-01-16 15:29:57 -0300

    :fire: FINAL

--- src/components/Chat/styles.js
+++ src/components/Chat/styles.js
@@ -3,7 +3,7 @@ import {Themes} from "../../styles/themes"
 import background from "../../assets/wallpaper.png"
 
 export const Container = styled.div`
-height: 100%;
+height: 93%;
 width: 100%;
 background-image: url(${background});
   background-repeat: repeat;
