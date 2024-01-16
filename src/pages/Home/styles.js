162e4beb3fc6d1b69ab8e880e3c1b8e9af7d4413
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2024-01-16 15:18:58 -0300

    :sparkles: Final ;)

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,7 +13,7 @@ export const WRapper = styled.div`
 `
 export const Container = styled.div`
 width: 80vw;
-height: 80%;
+height: 90%;
 background-color: ${Themes.darkmode};
 display: flex;
 flex-direction: row;
